90a9bd428ac3f7d534d8bfcd80c4d357e7623043
Sam Gruse <sgruse89@gmail.com>
2016-03-08 13:04:10 -0800

    Firt commit and I have completed this hw assignment

---
+++ response.js
@@ -0,0 +1,11 @@
+GET /favicon.ico HTTP/1.1
+Host: localhost:3000
+Connection: keep-alive
+Pragma: no-cache
+Cache-Control: no-cache
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36
+Accept: */*
+Referer: http://localhost:3000/
+Accept-Encoding: gzip, deflate, sdch
+Accept-Language: en-US,en;q=0.8
+
---
+++ test/test-server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var chai = require('chai'),
+chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+
+describe('Testing my TCP server response', ()=>{
+  it('Should responsd with a 200 status code', (done)=>{
+    chai.request('http://localhost:3000').get('/')
+    .end((err, response)=>{
+      expect(err).to.be(null);
+      expect(response).to.have.status(200);
+    });
+    done();
+  });
+});
