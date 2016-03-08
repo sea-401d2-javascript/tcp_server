05cc83e673ff7b9186ec6070cc9e20caa9752eb7
Ryan Flaherty <rcflaherty@gmail.com>
2016-03-08 12:38:29 -0800

    filecount test working

--- ryan-flaherty/test/test.js
+++ ryan-flaherty/test/test.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var net = require('net');
+var fs = require('fs');
 require('../index.js');
 
 describe('Testing TCP Server', () => {
@@ -13,6 +14,23 @@ describe('Testing TCP Server', () => {
     it('should respond with "You\'re connection has been logged!"', (done) => {
       client.on('data', function(data){
         expect(data).to.eql('You\'re connection has been logged!');
+      });
+      client.write('connect');
+      done();
+    });
+  });
+  describe('Logging the connection', () => {
+    var client;
+    before(function() {
+      client = net.connect(3000, function(){
+        client.write('connect');
+      });
+    });
+    it('Qty of logs should be 1 more than original count', (done) => {
+      var fileCount = fs.readdirSync('../');
+      client.on('data', function(data){
+        var fileCount2 = fs.readdirSync('../');
+        expect(fileCount2).to.eql(fileCount + 1);
       });
       client.write('connect');
       done();
