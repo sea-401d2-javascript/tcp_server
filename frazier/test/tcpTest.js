f830a0dc45933669e1c7b7f175bb76682b651989
Frazier Mork <fraziermork@gmail.com>
2016-03-08 11:33:52 -0800

    got test and requests saved--test only checks for a 200 status

--- frazier/test/tcpTest.js
+++ frazier/test/tcpTest.js
@@ -1,3 +1,19 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var expect = chai.expect;
 chai.use(chaiHttp);
+// var frazier-tcp = require(__dirname +'/../lib/tcp.js');
+
+describe('tcp.js', () => {
+  it('should respond with a 200 status', (done)=> {
+    chai.request('http://localhost:8000').get('/')
+    .end((err, response)=> {
+      // if (err) throw err;
+      expect(err).to.be(null);
+      expect(response).to.have.status(200);
+    });
+    
+    
+    done();
+  });
+});
