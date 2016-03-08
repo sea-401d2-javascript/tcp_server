7ef30d71c323714dc2e6e2b6c305fd059286350e
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-08 13:04:00 -0800

    refactored to use socket.on instead of event emitter

--- sawako_i_tcp/server.js
+++ sawako_i_tcp/server.js
@@ -1,33 +1,27 @@
 'use strict';
 const net = require('net');
 const fs = require('fs');
-const EventEmitter = require('events').EventEmitter;
-const emitter = new EventEmitter();
+// const EventEmitter = require('events').EventEmitter;
+// const emitter = new EventEmitter();
 
 net.createServer((socket) => {
 //creating date and time created here
-var now = new Date();
-var nowWrite = now.toISOString();
-var time = now.getTime();
-var nowTime = 'log/' + time + '.txt';
+  var now = new Date();
+  var nowWrite = now.toISOString();
+  var time = now.getTime();
+  var nowTime = 'log/' + time + '.txt';
 
 //uses the instance of time and date to create a file and write in it
-  emitter.emit('log',nowTime, nowWrite);
-  socket.write('HTTP/1.1 200');
-  socket.write('Content-Type: text/plain');
-  socket.write('hello world');
-  console.log('sever started!');
-//ending server
-  socket.end();
-}).listen(3000);
-
-//event emitter for creating file with time and date instance data
-emitter.on('log', (nowTime, nowWrite) =>{
-  fs.writeFile( __dirname + '/' + nowTime, nowWrite, (err)=>{
-    if(err){
-      console.log('Your data is not written anywhere..' + err);
-    } else {
-      console.log(nowTime + ' is wrote in a file')
-    }
+  socket.on('end', () =>{
+    fs.writeFile( __dirname + '/' + nowTime, nowWrite, (err)=>{
+      if(err){
+        console.log('Your data is not written anywhere..' + err);
+      } else {
+        console.log(nowTime + ' is wrote in a file');
+      }
+    });
   });
+  socket.end();
+}).listen(3000, () =>{
+  console.log('sever started!');
 });
--- sawako_i_tcp/test/test.js
+++ sawako_i_tcp/test/test.js
@@ -1,1 +1,15 @@
 'use strict';
+var expect = require('chai').expect;
+// var chaihttp = require('chai-http');
+
+// chaihttp.use(chaihttp);
+
+describe('Testing TCP server if it is connecting', () => {
+  it('should send response of status 200', () =>{
+    chaihttp.request('http://localhost3000')
+    .get('/')
+    .then((socket) =>{
+      expect(socket).to.have.status(300);
+    });
+  });
+});
