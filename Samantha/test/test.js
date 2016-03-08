b1c7996063cc9d10eb5bd81fa1e53b011c9242f3
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-08 12:30:04 -0800

    add tests

--- Samantha/test/test.js
+++ Samantha/test/test.js
@@ -7,20 +7,21 @@ require(__dirname + '/../tcp-server');
 var client = require(__dirname + '/../client');
 
 describe('Test the tcp-server', function(){
-  var contents;
+  var contents = 'initial data';
   var lastFile;
   before(function(done){
     client;
-    done();
     fs.readdir(__dirname + '/../logs/', (err, files) => {
       files.sort();
       lastFile = files[files.length-1];
     })
-    fs.readFile(__dirname + '/../logs/' + lastFile, (err, data)=>{
-      contents=data;
+    fs.readFile(__dirname + '/../logs/' + lastFile, (err, data) => {
+      contents = data;
     })
+    done();
   });
   it('should write a new file with a uniquie identifier based on clinet request', function(){
+    console.log(contents);
     expect(contents).to.eql('GET / HTTP/1.1 Host: localhost:3000 User-Agent: curl/7.46.0 Accept: */*')
   })
 })
