cd1223ef83ed264cddce392fd4a41921eee214ef
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-03-08 10:41:21 -0800

    created tcp server

--- brandon_feinstein/tcp-server.js
+++ brandon_feinstein/tcp-server.js
@@ -1,10 +1,15 @@
+'use strict';
 var net = require('net');
 var fs = require('fs');
 
-var server = net.createServer( (socket) => {
-
+var server = net.createServer( (socket) => { //socket is the connection that is sending and receiving data when the connection is received run the code below.
+  var file = fs.createWriteStream('./' + new Date());
+  socket.pipe(file);//piping info that browser is sending to server and writing that info to a file
+  socket.on('end', () => {
+    console.log('packet received');
+  });
 });
 
-server.listen('3000', () =>{
+server.listen('3000', () => { //port server is listening on
   console.log('server running');
 });
