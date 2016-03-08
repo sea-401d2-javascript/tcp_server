439eedc7170e075afa3ebe308bfe9c989c78eabb
Alemneh <Alemneh@hotmail.com>
2016-03-08 13:42:26 -0800

    updated gulpfile.js to reflect tcp_server.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var eslint = require('gulp-eslint');
 
-var files = ['gulpfile.js', 'bmp.js', '*.bmp', __dirname + '/lib/**/*.js', __dirname + '/test/**/*.js'];
+var files = ['gulpfile.js', 'tcp_server.js', '*.bmp', __dirname + '/lib/**/*.js', __dirname + '/test/**/*.js'];
 
 //Run mocha for tests
 gulp.task('mocha', function() {
