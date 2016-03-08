de633ac8dd26138db2883080dd7d676c22b5be18
Lex Myers <lex.myers@gmail.com>
2016-03-08 11:14:27 -0800

    added logging

--- lexm/server.js
+++ lexm/server.js
@@ -1,14 +1,27 @@
 'use strict';
 
+const fs = require('fs');
 const net = require('net');
+const moment = require('moment');
+
+var createUniqueName = function() {
+  return moment().format('YYYY-MM-DDTHH:mm:ss.SSSSSS') + '.log';
+}
+
+exports.createUniqueName = createUniqueName;
+
 var server = net.createServer( (socket) => {
+  var saveLog = '';
+  var logName = createUniqueName();
   socket.write('HTTP/1.1 200\n');
   socket.write('Content-Type: text/plain\n');
   socket.write('What\'s up?\n');
   socket.on('data', function(data) {
-    console.log(data.toString('utf-8'));
+    saveLog += data.toString('utf-8');
   });
   socket.on('end', function() {
+    fs.writeFile(__dirname + '/log/' + logName, saveLog);
+    console.log(saveLog);
     console.log('see ya!');
   })
 }).listen(3000);
--- lexm/test/server-test.js
+++ lexm/test/server-test.js
@@ -4,9 +4,9 @@ const net = require('net');
 const expect = require('chai').expect;
 const server = require(__dirname + '/../server.js');
 
-describe('testing createUnique', function() {
+describe('testing createUniqueName', function() {
   it('should be two different names', function() {
-    var name1 = server.createUnique();
-    expect(name1).to.not.equal(createUnique);
+    var name1 = server.createUniqueName();
+    expect(name1).to.not.equal(server.createUniqueName());
   })
 })
